c79a5ab8682fbbd6935be207b0087aabcf185787
Jerome Ona <jeromeona@Mac.attlocal.net>
2025-08-18 09:56:33 -0700

    Fix FB SDK typing and expression usage in Reviews; tidy page import

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,23 +1,30 @@
-"use client";
-import { useEffect } from "react";
+'use client';
+import { useEffect } from 'react';
+
+// Minimal typing for the FB SDK on window
+declare global {
+  interface Window {
+    FB?: { XFBML?: { parse: () => void } };
+  }
+}
 
 export default function Reviews() {
   useEffect(() => {
-    if (document.getElementById("fb-sdk")) {
-      // @ts-ignore
-      window.FB && window.FB.XFBML && window.FB.XFBML.parse();
+    const parse = () => window.FB?.XFBML?.parse();
+
+    if (document.getElementById('fb-sdk')) {
+      // SDK already present, (re)parse
+      parse();
       return;
     }
-    const s = document.createElement("script");
-    s.id = "fb-sdk";
+
+    const s = document.createElement('script');
+    s.id = 'fb-sdk';
     s.async = true;
     s.defer = true;
-    s.crossOrigin = "anonymous";
-    s.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0";
-    s.onload = () => {
-      // @ts-ignore
-      window.FB && window.FB.XFBML && window.FB.XFBML.parse();
-    };
+    s.crossOrigin = 'anonymous';
+    s.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0';
+    s.onload = parse; // call parse when loaded
     document.body.appendChild(s);
   }, []);
 
@@ -38,7 +45,14 @@ export default function Reviews() {
         />
       </div>
       <p className="muted" style={{ marginTop: 12 }}>
-        If reviews don’t load, <a href="https://www.facebook.com/search/top?q=guitar%20harbour%20custom%20shop" target="_blank" rel="noopener noreferrer">see them on Facebook</a>.
+        If reviews don’t load,{' '}
+        <a
+          href="https://www.facebook.com/search/top?q=guitar%20harbour%20custom%20shop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          see them on Facebook
+        </a>.
       </p>
     </div>
   );
