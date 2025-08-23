990bce86a0fdca3673ff6769833e72b8ebc2cd2e
Jerome Ona <ona.jerome@gmail.com>
2025-08-23 12:22:23 -0700

    fix header sub logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,25 +12,23 @@ export default function Header() {
           <ThemeToggle />
         </div>
 
-        {/* Logo block with grid layout */}
+        {/* Logo row: icon left, text block right */}
         <div className="logo" aria-label="Guitar Harbour">
-          <div className="logo-stack">
-            {/* Logo icons (theme toggle switches them) */}
-            <img
-              className="logo-dark"
-              src="/assets/logo.svg"
-              alt="Guitar Harbour logo"
-            />
-            <img
-              className="logo-light"
-              src="/assets/logo1.svg"
-              alt="Guitar Harbour logo"
-            />
+          {/* Theme-switched icons */}
+          <img
+            className="logo-dark"
+            src="/assets/logo.svg"
+            alt="Guitar Harbour logo"
+          />
+          <img
+            className="logo-light"
+            src="/assets/logo1.svg"
+            alt="Guitar Harbour logo"
+          />
 
-            {/* Wordmark */}
+          {/* Wordmark + subtitle stacked */}
+          <div className="logo-text">
             <span className="logo-word">Guitar Harbour</span>
-
-            {/* Subtitle */}
             <span className="logo-sub">
               Philippines&nbsp;| Los&nbsp;Angeles, CA
             </span>
@@ -49,18 +47,10 @@ export default function Header() {
             Menu
           </button>
           <ul id="menu" className={open ? "open" : ""}>
-            <li>
-              <a href="#services">Services</a>
-            </li>
-            <li>
-              <a href="#shop">Shop</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#shop">Shop</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
       </div>
