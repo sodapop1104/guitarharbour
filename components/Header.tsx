063658d5db9ada20a03601e0a07970fd907bbcad
Jerome Ona <ona.jerome@gmail.com>
2025-08-26 20:32:27 -0700

    added 'gallery' link

--- components/Header.tsx
+++ components/Header.tsx
@@ -50,6 +50,7 @@ export default function Header() {
             <li><a href="#services">Services</a></li>
             <li><a href="#shop">Shop</a></li>
             <li><a href="#about">About</a></li>
+            <li><a href="#gallery">Gallery</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
